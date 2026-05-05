d73c08da70bf1b37a911798387fbf45f1a432fdb
Vinay <vinayrath63020@gmail.com>
2026-05-05 11:32:26 +0530

    initial commit - JS week 1

--- week-1 js/script.js
+++ week-1 js/script.js
@@ -1,3 +1,10 @@
-function discountCalulator() {
-    
+const person = {
+    name: "Vinay",
+    age: 20,
+    city: "Hyderabad"
 }
+
+const { name, ...rest } = person
+
+console.log(name)
+console.log(rest)
